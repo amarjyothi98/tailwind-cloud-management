d000cf03f05971bcfadaa226c3f4ed12563c0d31
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-24 23:46:45 +0530

    installed tailwind successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      hello, new proj
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
