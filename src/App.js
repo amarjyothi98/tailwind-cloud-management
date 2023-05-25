90ece8d57be094cb9e1434157c8b85adad3b2dcc
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-25 22:30:00 +0530

    assets and components into the folder structure done:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 import './index.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Navbar/> 
+    </>
   );
 }
 
