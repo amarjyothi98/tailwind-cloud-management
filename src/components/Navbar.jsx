9eb324c13f9ec8506140ea103339162430fd5dfb
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-25 23:56:50 +0530

    added the navbar(almost) and added the screen size into the tailwind.config.js file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
   return (
-    <div>
-      this is navbar.
+    <>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+        <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='flex items-center'>
+                <h1 className='text-3x font-bold mr-4 sm:text-4xl '>BRAND.</h1>
+            </div>
+
+        </div>
+        <MenuIcon/>
+        this is navbar.
     </div>
+
+    </>
   )
 }
